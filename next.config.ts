c2df6d5b1b0d6b001d67335539ddd8f63461bb95
doge <doge@gmail.com>
2026-05-06 22:04:53 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve ??= {};
+    config.resolve.alias ??= {};
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
 };
 
 export default nextConfig;
